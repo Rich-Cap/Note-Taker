cb5bd8b6b598b8e65c79ba98cb42425d9e526af6
Rich Capuy <rccapuy@gmail.com>
2020-05-15 14:21:48 -0400

    routes directory

--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, function(){
 	console.log("App is listening on PORT: " + PORT);
 })
