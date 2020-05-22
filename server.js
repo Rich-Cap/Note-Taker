55fcb214b6dd6e5bbe0ae076fa5893a8c1b0645c
Rich Capuy <rccapuy@gmail.com>
2020-05-22 13:32:30 -0400

    final move

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-require("./Develop/routes/apiRoutes")(app);
-require("./Develop/routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function(){
 	console.log("App is listening on PORT: " + PORT);
