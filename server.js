a34768506d610eb41bbc879da373161444e3641d
Rich Capuy <rccapuy@gmail.com>
2020-05-22 13:28:03 -0400

    files moved

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+	console.log("App is listening on PORT: " + PORT);
+});
